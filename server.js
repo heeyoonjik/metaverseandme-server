8d306c0271df05f8f1c81aa9acf1424c59d3e5eb
heeyoon <heeyoon1302@naver.com>
2022-08-22 19:53:12 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.listen(process.env.PORT || 8080, function () {});
+
+app.get("/data", async (req, res) => {
+  let data = [
+    { name: "김철수", content: "안녕" },
+    { name: "남지수", content: "멋지다" },
+    { name: "박상민", content: "춥다" },
+    { name: "김희윤", content: "배불러" },
+  ];
+  res.send(data);
+});
